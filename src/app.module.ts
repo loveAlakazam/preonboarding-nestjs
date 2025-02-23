6718086845de2628023caf1de003b82ce4894aab
eunkang-choi <dmsrkd1216@gmail.com>
2025-02-23 09:21:06 +0900

    :wrench: Chore: 노드환경에 따라 옵션값 다르게 지정하도록 설정 & typeORM 옵션 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,56 @@
-import { Module, OnModuleInit } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
+import { Module, OnModuleInit } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { UsersModule } from "@users/users.module";
+import { BoardsModule } from "./modules/boards/boards.module";
+import { CommentsController } from "./modules/comments/controllers/comments.controller";
+import { CommentsModule } from "./modules/comments/comments.module";
+import { UserEntity } from "@users/entity/users.entity";
+import { BoardEntity } from "@boards/entity/boards.entity";
+import { CommentEntity } from "@comments/entity/comments.entity";
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: (() => {
+        switch (process.env.NODE_ENV) {
+          case "production":
+            return "production.env";
+          default:
+            return ".env";
+        }
+      })(),
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get<string>("DB_HOST", "localhost"),
+        port: configService.get<number>("DB_PORT", 3306),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_NAME"),
+        synchronize:
+          configService.get<string>("NODE_ENV") === "production" ? false : true,
+        logging:
+          configService.get<string>("NODE_ENV") === "production" ? false : true,
+        entities: [UserEntity, BoardEntity, CommentEntity],
+        // dropSchema: configService.get<string>("NODE_ENV") === "production" ? false : true,
+      }),
+      inject: [ConfigService], // ConfigService 주입
+    }),
+    UsersModule,
+    BoardsModule,
+    CommentsModule,
+  ],
+  controllers: [AppController, CommentsController],
   providers: [AppService],
 })
 export class AppModule implements OnModuleInit {
   onModuleInit() {
-    console.log('The module has been initialized');
+    console.log("The module has been initialized");
   }
 }
