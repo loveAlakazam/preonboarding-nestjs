8fb7a92dbdcd03c9086f39fb9efe30f36736d060
eunkang-choi <dmsrkd1216@gmail.com>
2025-02-23 09:16:31 +0900

    :sparkles: Feature: user 모듈 구성요소 추가
    컨트롤러/서비스/엔티티 추가

---
+++ src/modules/users/entity/users.entity.ts
@@ -0,0 +1,29 @@
+import { Exclude } from "class-transformer";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity } from "@src/commons/entity/base.entity";
+import { BoardEntity } from "@boards/entity/boards.entity";
+import { CommentEntity } from "@comments/entity/comments.entity";
+
+@Entity({
+  name: "users",
+})
+export class UserEntity extends BaseEntity {
+  // mysql에서는 BIGINT 타입으로 저장되지만, typescript에서는 string으로 다룸.
+  @PrimaryGeneratedColumn("increment", { type: "bigint" })
+  id: string;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  // users:boards=1:N
+  @OneToMany(() => BoardEntity, (board) => board.user)
+  boards: BoardEntity[];
+
+  // users:comments=1:N
+  @OneToMany(() => CommentEntity, (comment) => comment.user)
+  comments: CommentEntity[];
+}
