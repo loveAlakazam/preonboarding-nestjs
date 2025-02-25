39684d9f1382e63dc793728685f6c49f30ad0353
eunkang-choi <dmsrkd1216@gmail.com>
2025-02-26 00:33:22 +0900

    :wrench: Chore: 글로벌파이프 추가
    유효성검사 class-validator 에 사용되는 파이프 ValidationPipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { BadRequestException, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // dto에 정의되지 않은 속성 제거
+      forbidNonWhitelisted: true, // 정의되지 않은 속성이 들어오면 예외발생
+      transform: true, // dto 내부에서 자동변환 적용
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
