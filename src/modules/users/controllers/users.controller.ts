f1b8e7c4d83c58c8a17e58536e0ae78fc3b3a814
eunkang-choi <dmsrkd1216@gmail.com>
2025-02-26 00:38:44 +0900

    :white_check_mark: Test: 유저컨트롤러 유효성검사관련 실패 테스트케이스 6개 추가
    실패테스트케이스 - nickname(2) / password(3) / passwordConfirm(1) 유효성검사 관련 테스트케이스 추가

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Inject, Post } from "@nestjs/common";
+import { UsersService } from "../services/users.service";
+import { CreateNewUserRequestDto } from "../dto/create-new-user-request.dto";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  constructor(
+    @Inject()
+    private userService: UsersService,
+  ) {}
+
+  @Post("sign-up")
+  @HttpCode(201)
+  async createNewUser(@Body() requestDto: CreateNewUserRequestDto) {
+    return await this.userService.createNewUser(requestDto);
+  }
+}
