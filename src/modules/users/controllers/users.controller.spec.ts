37a58390a24206239e872a31a29f4a870ac6ad5e
eunkang-choi <dmsrkd1216@gmail.com>
2025-02-26 18:19:39 +0900

    ✅ Test: 유저로그인 API 유효성검사 테스트케이스 작성

--- src/modules/users/controllers/users.controller.spec.ts
+++ src/modules/users/controllers/users.controller.spec.ts
@@ -7,13 +7,17 @@ import {
   USER_NICKNAME_MIN_LENGTH,
   USER_PASSWORD_MIN_LENGTH,
 } from "@users/constants/users.constant";
-import { MINLENGTH } from "@src/commons/errors/commons.error-message";
+import {
+  MINLENGTH,
+  SHOULD_BE_EXIST,
+} from "@src/commons/errors/commons.error-message";
 import {
   ALLOW_ALPHABET_AND_NUMBER,
   PASSWORD_CONTAINS_NICKNAME,
   PASSWORD_NOT_EQUALS_TO_CONFIRMED_PASSWORD,
 } from "../errors/users.error-message";
 import * as request from "supertest";
+import { LoginUserRequestDto } from "../dto/login-user.request.dto";
 
 describe("UsersController (request validation)", () => {
   let app: INestApplication;
@@ -51,105 +55,123 @@ describe("UsersController (request validation)", () => {
     await app.close();
   });
 
-  describe("nickname", () => {
-    it("nickname 길이가 최소글자 미만일 경우 유효성검사에 실패해야한다.", async () => {
-      const invalidRequestDto: CreateNewUserRequestDto = {
-        nickname: "a", // invalid
-        password: rightRequestDto.password,
-        passwordConfirm: rightRequestDto.passwordConfirm,
+  describe("sign-in", () => {
+    it("nickname 데이터가 비어있으면 유효성검사에 실패한다", async () => {
+      const invalidRequestDto: LoginUserRequestDto = {
+        nickname: "",
+        password: "password1234",
       };
 
       const response = await request(app.getHttpServer())
-        .post("/users/sign-up")
+        .post("/users/sign-in")
         .send(invalidRequestDto);
 
       expect(response.status).toBe(400);
-      expect(response.body.message).toContain(
-        MINLENGTH("nickname", USER_NICKNAME_MIN_LENGTH),
-      );
-    });
-
-    it("nickname 이 알파벳+숫자 조합이 아닌 경우 유효성검사에 실패해야한다.", async () => {
-      const invalidRequestDto: CreateNewUserRequestDto = {
-        nickname: "테스트1234", // invalid
-        password: rightRequestDto.password,
-        passwordConfirm: rightRequestDto.passwordConfirm,
-      };
-
-      const response = await request(app.getHttpServer())
-        .post("/users/sign-up")
-        .send(invalidRequestDto);
-
-      expect(response.status).toBe(400);
-      expect(response.body.message).toContain(
-        ALLOW_ALPHABET_AND_NUMBER("nickname"),
-      );
+      expect(response.body.message).toContain(SHOULD_BE_EXIST("nickname"));
     });
   });
 
-  describe("password", () => {
-    it("password 길이가 최소글자 미만일 경우 유효성검사에 실패해야한다.", async () => {
-      const invalidRequestDto: CreateNewUserRequestDto = {
-        nickname: "testUser",
-        password: "a1", // invalid
-        passwordConfirm: "a1",
-      };
+  describe("sign-up", () => {
+    describe("nickname", () => {
+      it("nickname 길이가 최소글자 미만일 경우 유효성검사에 실패해야한다.", async () => {
+        const invalidRequestDto: CreateNewUserRequestDto = {
+          nickname: "a", // invalid
+          password: rightRequestDto.password,
+          passwordConfirm: rightRequestDto.passwordConfirm,
+        };
 
-      const response = await request(app.getHttpServer())
-        .post("/users/sign-up")
-        .send(invalidRequestDto);
+        const response = await request(app.getHttpServer())
+          .post("/users/sign-up")
+          .send(invalidRequestDto);
 
-      expect(response.status).toBe(400);
-      expect(response.body.message).toContain(
-        MINLENGTH("password", USER_PASSWORD_MIN_LENGTH),
-      );
+        expect(response.status).toBe(400);
+        expect(response.body.message).toContain(
+          MINLENGTH("nickname", USER_NICKNAME_MIN_LENGTH),
+        );
+      });
+
+      it("nickname 이 알파벳+숫자 조합이 아닌 경우 유효성검사에 실패해야한다.", async () => {
+        const invalidRequestDto: CreateNewUserRequestDto = {
+          nickname: "테스트1234", // invalid
+          password: rightRequestDto.password,
+          passwordConfirm: rightRequestDto.passwordConfirm,
+        };
+
+        const response = await request(app.getHttpServer())
+          .post("/users/sign-up")
+          .send(invalidRequestDto);
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toContain(
+          ALLOW_ALPHABET_AND_NUMBER("nickname"),
+        );
+      });
     });
-    it("password 가 알파벳+숫자 조합이 아닌 경우 유효성검사에 실패해야한다.", async () => {
-      const invalidRequestDto: CreateNewUserRequestDto = {
-        nickname: rightRequestDto.nickname,
-        password: "비밀번호1234", // invalid
-        passwordConfirm: "비밀번호1234",
-      };
 
-      const response = await request(app.getHttpServer())
-        .post("/users/sign-up")
-        .send(invalidRequestDto);
+    describe("password", () => {
+      it("password 길이가 최소글자 미만일 경우 유효성검사에 실패해야한다.", async () => {
+        const invalidRequestDto: CreateNewUserRequestDto = {
+          nickname: "testUser",
+          password: "a1", // invalid
+          passwordConfirm: "a1",
+        };
 
-      expect(response.status).toBe(400);
-      expect(response.body.message).toContain(
-        ALLOW_ALPHABET_AND_NUMBER("password"),
-      );
-    });
+        const response = await request(app.getHttpServer())
+          .post("/users/sign-up")
+          .send(invalidRequestDto);
 
-    it("password 에 nickname 이 포함되면 유효성검사에 실패해야한다.", async () => {
-      const invalidRequestDto: CreateNewUserRequestDto = {
-        nickname: "testUser",
-        password: "testUser1234", // invalid
-        passwordConfirm: "testUser1234",
-      };
+        expect(response.status).toBe(400);
+        expect(response.body.message).toContain(
+          MINLENGTH("password", USER_PASSWORD_MIN_LENGTH),
+        );
+      });
+      it("password 가 알파벳+숫자 조합이 아닌 경우 유효성검사에 실패해야한다.", async () => {
+        const invalidRequestDto: CreateNewUserRequestDto = {
+          nickname: rightRequestDto.nickname,
+          password: "비밀번호1234", // invalid
+          passwordConfirm: "비밀번호1234",
+        };
 
-      const response = await request(app.getHttpServer())
-        .post("/users/sign-up")
-        .send(invalidRequestDto);
+        const response = await request(app.getHttpServer())
+          .post("/users/sign-up")
+          .send(invalidRequestDto);
 
-      expect(response.status).toBe(400);
-      expect(response.body.message).toContain(PASSWORD_CONTAINS_NICKNAME);
-    });
-    it("password 와 passwordConfirm 이 일치하지 않으면 유효성검사에 실패해야한다.", async () => {
-      const invalidRequestDto: CreateNewUserRequestDto = {
-        nickname: "helloworld",
-        password: "testUser1234",
-        passwordConfirm: "testUser1235", // invalid
-      };
+        expect(response.status).toBe(400);
+        expect(response.body.message).toContain(
+          ALLOW_ALPHABET_AND_NUMBER("password"),
+        );
+      });
 
-      const response = await request(app.getHttpServer())
-        .post("/users/sign-up")
-        .send(invalidRequestDto);
+      it("password 에 nickname 이 포함되면 유효성검사에 실패해야한다.", async () => {
+        const invalidRequestDto: CreateNewUserRequestDto = {
+          nickname: "testUser",
+          password: "testUser1234", // invalid
+          passwordConfirm: "testUser1234",
+        };
 
-      expect(response.status).toBe(400);
-      expect(response.body.message).toContain(
-        PASSWORD_NOT_EQUALS_TO_CONFIRMED_PASSWORD,
-      );
+        const response = await request(app.getHttpServer())
+          .post("/users/sign-up")
+          .send(invalidRequestDto);
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toContain(PASSWORD_CONTAINS_NICKNAME);
+      });
+      it("password 와 passwordConfirm 이 일치하지 않으면 유효성검사에 실패해야한다.", async () => {
+        const invalidRequestDto: CreateNewUserRequestDto = {
+          nickname: "helloworld",
+          password: "testUser1234",
+          passwordConfirm: "testUser1235", // invalid
+        };
+
+        const response = await request(app.getHttpServer())
+          .post("/users/sign-up")
+          .send(invalidRequestDto);
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toContain(
+          PASSWORD_NOT_EQUALS_TO_CONFIRMED_PASSWORD,
+        );
+      });
     });
   });
 });
--- src/modules/users/dto/login-user.request.dto.ts
+++ src/modules/users/dto/login-user.request.dto.ts
@@ -1,4 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { SHOULD_BE_EXIST } from "@src/commons/errors/commons.error-message";
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class LoginUserRequestDto {
+  /** 닉네임 */
+  @IsNotEmpty({ message: SHOULD_BE_EXIST("nickname") })
+  @IsString()
+  @ApiProperty({ description: "닉네임", example: "test123" })
   nickname: string;
+
+  /** 비밀번호 */
+  @IsNotEmpty({ message: SHOULD_BE_EXIST("password") })
+  @IsString()
+  @ApiProperty({ description: "비밀번호", example: "secretValue1" })
   password: string;
 }
