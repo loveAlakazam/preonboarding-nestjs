8c5437dc42039d6465139f8bf4cc7d47032a2402
eunkang-choi <dmsrkd1216@gmail.com>
2025-03-10 13:13:10 +0900

    🧪Test: 댓글생성  컨트롤러 테스트케이스 작성 및 dto 에러문구 추가

--- src/modules/comments/controllers/comments.controller.spec.ts
+++ src/modules/comments/controllers/comments.controller.spec.ts
@@ -1,18 +1,146 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { CommentsController } from "./comments.controller";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { BoardRepository } from "@boards/repositories/boards.repository";
+import { UserRepository } from "@users/repositories/users.repository";
+import { CommentsController } from "@comments/controllers/comments.controller";
+import { CommentsService } from "@comments/services/comments.service";
+import { CommentRepository } from "@comments/repositories/comments.repository";
+import * as request from "supertest";
+import {
+  SHOULD_BE_EXIST,
+  SHOULD_BE_STRING,
+} from "@src/commons/errors/commons.error-message";
 
 describe("CommentsController", () => {
-  let controller: CommentsController;
+  let app: INestApplication;
+  let commentService: CommentsService;
+  let commentRepository: CommentRepository;
+  let boardRepository: BoardRepository;
+  let userRepository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentsController],
+      providers: [
+        CommentsService,
+        {
+          provide: CommentRepository,
+          useValue: {
+            create: jest.fn(),
+            findOneById: jest.fn(),
+            update: jest.fn(),
+            findCommentsByBoardId: jest.fn(),
+            delete: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        {
+          provide: BoardRepository,
+          useValue: {
+            findOneById: jest.fn(),
+          },
+        },
+        {
+          provide: UserRepository,
+          useValue: {
+            findOneById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<CommentsController>(CommentsController);
+    commentService = module.get<CommentsService>(CommentsService);
+    commentRepository = module.get<CommentRepository>(CommentRepository);
+    boardRepository = module.get<BoardRepository>(BoardRepository);
+    userRepository = module.get<UserRepository>(UserRepository);
+    app = module.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    await app.init();
   });
 
-  it("should be defined", () => {
-    expect(controller).toBeDefined();
+  afterEach(async () => {
+    await app.close();
+  });
+
+  describe("CommentController", () => {
+    /**
+     * 댓글 생성
+     */
+    describe("createNewComment", () => {
+      describe("boardId", () => {
+        it("boardId 가 존재하지 않으면, BadRequestException 예외가 발생한다", async () => {
+          const invalidRequest = {
+            boardId: "", // invalid
+            userId: "1",
+            content: "",
+          };
+
+          const response = await request(app.getHttpServer())
+            .post("/comments/")
+            .send(invalidRequest);
+
+          expect(response.status).toBe(400);
+          expect(response.body.message).toContain(SHOULD_BE_EXIST("boardId"));
+        });
+        it("boardId 의 타입이 string이 아니라면, BadRequestException 예외가 발생한다", async () => {
+          const invalidRequest = {
+            boardId: true, // invalid
+            userId: "1",
+            content: "",
+          };
+
+          const response = await request(app.getHttpServer())
+            .post("/comments/")
+            .send(invalidRequest);
+
+          expect(response.status).toBe(400);
+          expect(response.body.message).toContain(SHOULD_BE_STRING("boardId"));
+        });
+      });
+      describe("userId", () => {
+        it("userId가 존재하지 않으면 BadRequestException 예외가 발생한다", async () => {
+          const invalidRequest = {
+            boardId: "1",
+            userId: undefined, // invalid
+            content: "",
+          };
+
+          const response = await request(app.getHttpServer())
+            .post("/comments/")
+            .send(invalidRequest);
+
+          expect(response.status).toBe(400);
+          expect(response.body.message).toContain(SHOULD_BE_EXIST("userId"));
+        });
+        it("userId 의 타입이 string이 아니라면, BadRequestException 예외가 발생한다", async () => {
+          const invalidRequest = {
+            boardId: "1",
+            userId: 1, // invalid
+            content: "",
+          };
+
+          const response = await request(app.getHttpServer())
+            .post("/comments/")
+            .send(invalidRequest);
+
+          expect(response.status).toBe(400);
+          expect(response.body.message).toContain(SHOULD_BE_STRING("userId"));
+        });
+      });
+    });
+    /**
+     * 댓글 수정
+     */
+    describe("updateComment", () => {});
+    /**
+     * 댓글 삭제
+     */
+    describe("updateComment", () => {});
   });
 });
