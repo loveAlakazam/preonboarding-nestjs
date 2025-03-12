6dad8268ee420bef5a8f0b1b02c44d91e65cdf5a
eunkang-choi <dmsrkd1216@gmail.com>
2025-03-12 22:31:11 +0900

    :white_check_mark: Test: users.service 테스트케이스 수정
    jwt검증로직 추가후 로그인 테스트케이스 수정

--- test/modules/users/users.service.spec.ts
+++ test/modules/users/users.service.spec.ts
@@ -1,17 +1,26 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { JwtService } from "@nestjs/jwt";
+import {
+  BadRequestException,
+  HttpException,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { UsersService } from "@users/services/users.service";
 import { UserRepository } from "@users/repositories/users.repository";
 import { CreateNewUserRequestDto } from "@users/dto/create-new-user-request.dto";
-import { BadRequestException, HttpException } from "@nestjs/common";
 import {
   ALREADY_EXIST_USER,
   LOGIN_FAILED,
+  NOT_FOUND_USER,
 } from "@users/errors/users.error-message";
 import { LoginUserRequestDto } from "@users/dto/login-user.request.dto";
+import { TOKEN_FAILED } from "@auth/auth.error-message";
 
 describe("UsersService", () => {
   let userService: UsersService;
   let userRepository: UserRepository;
+  let jwtService: JwtService;
 
   const sampleUserData = {
     nickname: "testUser",
@@ -32,11 +41,18 @@ describe("UsersService", () => {
             }),
           },
         },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     userService = module.get<UsersService>(UsersService);
     userRepository = module.get<UserRepository>(UserRepository);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   afterEach(() => {
@@ -53,7 +69,7 @@ describe("UsersService", () => {
 
       // 비밀번호 불일치시 예외발생
       await expect(userService.loginUser(invalidRequest)).rejects.toThrow(
-        new BadRequestException(LOGIN_FAILED),
+        new NotFoundException(NOT_FOUND_USER),
       );
     });
     it("비밀번호가 일치하지 않으면 예외가 발생하여 회원 로그인이 실패된다.", async () => {
@@ -75,13 +91,33 @@ describe("UsersService", () => {
         new BadRequestException(LOGIN_FAILED),
       );
     });
-    it("계정정보가 일치하면 회원로그인을 성공하며 예외없이 정상종료 해야한다", async () => {
+    it("인증정보 검증후 jwtService에서 토큰생성에 오류가 발생하면, UnauthorizedException을 발생시킨다", async () => {
       // Mock 설정: 계정정보
       (userRepository.findOneByNickname as jest.Mock).mockResolvedValue({
         id: "1",
         nickname: sampleUserData.nickname,
         password: sampleUserData.password,
       });
+      (jwtService.signAsync as jest.Mock).mockReturnValue(null);
+
+      // 로그인 요청 dto
+      const request: LoginUserRequestDto = { ...sampleUserData };
+
+      // 계정일치시 리턴타입없이 종료
+      await expect(userService.loginUser(request)).rejects.toThrow(
+        new UnauthorizedException(TOKEN_FAILED),
+      );
+    });
+    it("계정정보가 일치하면 회원로그인을 성공하며, accessToken을 리턴한다", async () => {
+      // Mock 설정: 계정정보
+      (userRepository.findOneByNickname as jest.Mock).mockResolvedValue({
+        id: "1",
+        nickname: sampleUserData.nickname,
+        password: sampleUserData.password,
+      });
+      (jwtService.signAsync as jest.Mock).mockResolvedValue(
+        "loginSuccessAccessToken1234",
+      );
 
       // 로그인 요청 dto
       const request: LoginUserRequestDto = { ...sampleUserData };
