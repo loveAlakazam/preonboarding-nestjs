7f7053dccbde7c5849164c7046ee7bd1726c68f0
eunkang-choi <dmsrkd1216@gmail.com>
2025-03-16 03:35:57 +0900

    :wrench: Chore: package.json에 typeorm migration 관련 명령어 추가
    1) 명령어 추가: migration:generate/create/run/revert 명령어 추가  2) 마이그레이션파일위치 수정(mysql.datasource.ts와 동일한 위치로 디렉터리변경)

--- src/database/mysql.datasource.ts
+++ src/database/mysql.datasource.ts
@@ -2,12 +2,11 @@ import { BoardEntity } from "@src/modules/boards/entity/boards.entity";
 import { CommentEntity } from "@src/modules/comments/entity/comments.entity";
 import { UserEntity } from "@src/modules/users/entity/users.entity";
 import * as dotenv from "dotenv";
-import * as path from "path";
 import { DataSource } from "typeorm";
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST ?? "localhost",
   port: +(process.env.DB_PORT ?? 3306),
@@ -17,7 +16,7 @@ export const AppDataSource = new DataSource({
   logging: process.env.NODE_ENV === "production" ? false : true,
   synchronize: false,
   entities: [UserEntity, BoardEntity, CommentEntity],
-  migrations: [path.join(__dirname, "..", "migrations") + "/*.{js,ts}"],
+  migrations: [__dirname + "/migrations/*.{js,ts}"],
   migrationsRun: false,
 });
 
